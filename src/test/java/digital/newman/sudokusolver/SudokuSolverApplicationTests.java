39f7637211c085edc5df3e4685db8bb3e2b2a803
Stephen Newman <steve@newman.digital>
2021-01-28 21:52:33 +0000

    Initial commit

---
+++ src/test/java/digital/newman/sudokusolver/SudokuSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package digital.newman.sudokusolver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
